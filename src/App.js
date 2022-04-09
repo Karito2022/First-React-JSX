980082c114acd0b19468fa4fb74a77543fd788bd
Karito2022 <reyesbarrerakaren@gmail.com>
2022-04-09 18:35:27 -0500

    Finalizado

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import List from "./components/List"
+
+function App() {
+return (
+    <div className='App'>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <List />
+    </div>
+);
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import "./list.css";
+
+function List() {
+    return (
+        <ul className="list8x">
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    );
+}
+
+export default List;
